ddd9f1471172b82f792b2d85383d5cd84b7cac0a
Peter Hewitt <peter@hewittsoft.com>
2014-09-22 06:55:26 -0400

    Cleanup and added void statement function.

--- src/cljs/com/hewittsoft/twenty48/tincan.cljs
+++ src/cljs/com/hewittsoft/twenty48/tincan.cljs
@@ -8,6 +8,48 @@
             [cljs-time.core :as time]
             [cljs-time.format :as time-format]))
 
+(def twenty48-statement-template {
+    :actor {
+        :objectType "Agent"
+    }
+    :verb {
+    }
+    :object {
+        :id "http://hewittsoft.com/2048/"
+        :objectType "Activity"
+        :definition {
+            :name {
+                :en-US "2048 Clojurescript TinCan Demo"
+            }
+        }
+    }
+    :context {
+        :contextActivities {
+            :grouping [{
+                :id "http://hewittsoft.com/2048/"
+                :objectType "Activity"
+            }]
+        }
+    }
+})
+
+(def void-statement-template {
+    :actor {
+        :objectType "Agent"
+    }
+    :verb {
+        :id "http://adlnet.gov/expapi/verbs/voided"
+    }
+    :object {
+        :objectType "StatementRef"
+        :id nil
+    }
+})
+
+
+(def verb-scored { :id "http://adlnet.gov/expapi/verbs/scored" :display { :en-US "scored" }})
+(def verb-completed { :id "http://adlnet.gov/expapi/verbs/completed" :display { :en-US "completed" }})
+
 (defn- get-uuid []
   "get a new random uuid"
   (.-uuid (uuid/make-random)))
@@ -38,96 +80,39 @@
         statement (assoc statement :id statement-id :timestamp timestamp)]
     (http/put endpoint (create-tincan-request query-params statement))))
 
+(defn void-statement [name email stmt-id]
+  "void a statement from LRS using tincan API 1.0.1"
+  (let [void-statement (-> void-statement-template
+                           (assoc-in [:object :id] stmt-id)
+                           (update-in [:actor] assoc :name name :mbox (str "mailto:" email)))]
+    (send-statement void-statement)))
 
 (defn submit-high-score [game-id name email score]
   "submit a high score, by sending over tincan to a LRS"
   (send-statement
-   {
-      :actor {
-          :objectType "Agent"
-          :name name
-          :mbox (str "mailto:" email)
-      }
-      :verb {
-          :id "http://adlnet.gov/expapi/verbs/scored"
-          :display {
-              :en-US "scored"
-          }
-      }
-      :object {
-          :id "http://hewittsoft.com/2048/"
-          :objectType "Activity"
-          :definition {
-              :name {
-                  :en-US "2048 Clojurescript TinCan Demo"
-              }
-          }
-      }
-      :result {
-          :score {
-              :raw score
-          }
-      }
-      :context {
-          :contextActivities {
-              :grouping [{
-                  :id "http://hewittsoft.com/2048/"
-                  :objectType "Activity"
-              }]
-          }
-          :extensions {
-              "http://hewittsoft.com/2048/gameId" game-id
-          }
-      }}))
+    (-> twenty48-statement-template
+        (update-in [:actor] assoc :name name :mbox (str "mailto:" email))
+        (assoc-in [:context :extensions] { "http://hewittsoft.com/2048/gameId" game-id})
+        (assoc :result { :score { :raw score }} :verb verb-scored))))
 
 (defn submit-win [game-id name email]
   "submit a win"
   (send-statement
-    {
-      :actor {
-          :objectType "Agent"
-          :name name
-          :mbox (str "mailto:" email)
-      }
-      :verb {
-          :id "http://adlnet.gov/expapi/verbs/completed"
-          :display {
-              :en-US "completed"
-          }
-      }
-      :object {
-          :id "http://hewittsoft.com/2048/"
-          :objectType "Activity"
-          :definition {
-              :name {
-                  :en-US "2048 Clojurescript TinCan Demo"
-              }
-          }
-      }
-      :context {
-          :contextActivities {
-              :grouping [{
-                  :id "http://hewittsoft.com/2048/"
-                  :objectType "Activity"
-              }]
-          }
-          :extensions {
-              "http://hewittsoft.com/2048/gameId" game-id
-          }
-      }}))
+    (-> twenty48-statement-template
+        (update-in [:actor] assoc :name name :mbox (str "mailto:" email))
+        (assoc-in [:context :extensions] { "http://hewittsoft.com/2048/gameId" game-id})
+        (assoc :verb verb-completed))))
 
 
 (defn get-winners [callback]
-  (go (let [response (<! (http/get (str (:endpoint config/record-store) "statements")
-                                   {:query-params {:verb "http://adlnet.gov/expapi/verbs/completed"
-                                                   :activity "http://hewittsoft.com/2048/"}
-                                    :headers { "X-Experience-API-Version" "1.0.1" }
-                                    :basic-auth {:username (:username config/record-store)
-                                                 :password (:password config/record-store)}
-                                    :with-credentials? false}))
+  (go (let [endpoint (str (:endpoint config/record-store) "statements")
+            query-params {:verb "http://adlnet.gov/expapi/verbs/completed"
+                          :activity "http://hewittsoft.com/2048/"}
+            response (<! (http/get endpoint (create-tincan-request query-params nil)))
             err (if (:success response)  nil "Error getting winners")
             winners (if err nil
                       (->> (:statements (:body response))
+                           (filter #(= "http://adlnet.gov/expapi/verbs/completed" (:id (:verb %)))) ; ignore voided completions
                            (map #(hash-map :name (:name (:actor %)) :date (:timestamp %)))
                            (take 10)))]
         (callback err winners))))
